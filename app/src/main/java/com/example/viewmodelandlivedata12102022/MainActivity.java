a1f9d42afc6fcd62e8b5568dcd67d8a1538f8ee7
Phat <phatdroid94@gmail.com>
2022-12-19 19:32:17 +0700

    livedata

--- app/src/main/java/com/example/viewmodelandlivedata12102022/MainActivity.java
+++ app/src/main/java/com/example/viewmodelandlivedata12102022/MainActivity.java
@@ -1,64 +1,36 @@
 package com.example.viewmodelandlivedata12102022;
 
-import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.concurrent.CompletableFuture;
-import java.util.function.Consumer;
+import androidx.lifecycle.Observer;
 
 public class MainActivity extends AppCompatActivity {
 
+    MyLiveData myLiveData;
+    int count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-            handleNumber(5, 10)
-                    .thenAccept(new Consumer<Integer>() {
-                        @Override
-                        public void accept(Integer integer) {
-                            Log.d("BBB", integer + " ");
-                        }
-                    });
-        }
-    }
+        myLiveData = new MyLiveData();
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    private CompletableFuture<Integer> handleNumber(int numberA, int numberB) {
-        CompletableFuture<Integer> completableFuture = new CompletableFuture<>();
-        new Thread(new Runnable() {
+        myLiveData.getString().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                Log.d("BBB", s);
+            }
+        });
+
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                try {
-                    Thread.sleep(5000);
-                    completableFuture.complete(numberA + numberB);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                myLiveData.setData(String.valueOf(count++));
+                new Handler().postDelayed(this::run, 1000);
             }
-        }).start();
-        return completableFuture;
+        }, 1000);
     }
 
-    private void handleNumber1(int numberA, int numberB, OnListener onListener) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(5000);
-                    onListener.onDataChange(numberA + numberB);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
-    }
-
-    interface OnListener {
-        void onDataChange(int number);
-    }
 }
