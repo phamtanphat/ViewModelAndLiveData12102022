4ea3c35a4d667549ebf0973f28d62bceef195a1a
Phat <phatdroid94@gmail.com>
2022-12-21 17:59:38 +0700

    update

--- app/src/main/java/com/example/viewmodelandlivedata12102022/MainActivity.java
+++ app/src/main/java/com/example/viewmodelandlivedata12102022/MainActivity.java
@@ -3,34 +3,34 @@ package com.example.viewmodelandlivedata12102022;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 
 public class MainActivity extends AppCompatActivity {
 
-    MyLiveData myLiveData;
-    int count = 0;
+    EditText edtInput;
+    Button btnGetText;
+    TextView tvOutput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myLiveData = new MyLiveData();
 
-        myLiveData.getString().observe(this, new Observer<String>() {
+        edtInput = findViewById(R.id.edit_text);
+        btnGetText = findViewById(R.id.button_get_text);
+        tvOutput = findViewById(R.id.text_view_output);
+
+        btnGetText.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(String s) {
-                Log.d("BBB", s);
+            public void onClick(View view) {
+
             }
         });
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                myLiveData.setData(String.valueOf(count++));
-                new Handler().postDelayed(this::run, 1000);
-            }
-        }, 1000);
     }
 
 }
---
+++ app/src/main/java/com/example/viewmodelandlivedata12102022/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.example.viewmodelandlivedata12102022;
+
+/**
+ * Created by pphat on 12/21/2022.
+ */
+public class MainViewModel {
+}
